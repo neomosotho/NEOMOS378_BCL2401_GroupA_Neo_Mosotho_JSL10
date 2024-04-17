a05d457bd2f492eba246735cbb913064e3c5d452
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-04-17 15:09:13 +0200

    Adding await in Room 3

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -20,18 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // 🪲 Bug: Asynchronous function ?
-    document.getElementById("solveRoom3").addEventListener("click", () => {
-        fetch('directions.json') 
-            .then(response => response.json())
-            .then(directions => {
-                navigateLabyrinth(directions)
-                    .then(message => {
-                        // 🪲 Bug: Incorrect method
-                        document.getElementById("room3Result").innerHTML = message;
-                    });
-            });
+    document.getElementById("solveRoom3").addEventListener("click", async () => {
+    try {
+        const res = await fetch('directions.json') 
+        const directions = await res.json()
+        const message = await navigateLabyrinth(directions)
+        // 🪲 Bug: Incorrect method
+        document.getElementById("room3Result").innerHTML = message;
+    } catch (err) {
+        console.error("Error:",err)
+    }
     });
-});
+            
 
 function findMostRecentBook(books) {
     // 🪲 Bug: Logic error
