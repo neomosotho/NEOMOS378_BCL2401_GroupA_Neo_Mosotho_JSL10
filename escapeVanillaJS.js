10d1e7ae09fe2498eeb6c618bf63941bda1bf2e8
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-04-18 16:57:00 +0200

    Adding comments

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -1,25 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     // 🪲 Bug: Incorrect ID used for attaching the event listener
+    // Debugged the element ID used in the eventlistener
     document.getElementById("solveRoom1").addEventListener("click", () => {
         fetch('books.json') 
             .then(response => response.json())
             .then(books => {
                 const mostRecentBook = findMostRecentBook(books);
                 // 🪲 Bug: Incorrect element ID
+                // Corrected the element ID
                 document.getElementById("room1Result").textContent = `The key to the next room is: ${mostRecentBook.title}`;
             });
     });
 
     document.getElementById("solveRoom2").addEventListener("click", () => {
+        // Added an async to the jsConcepts
         const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'async']);
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async',]);
         // 🪲 Bug: Incorrect function call
+        // Added the correct functional cancelIdleCallback(reactConcepts)
         const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
     // 🪲 Bug: Asynchronous function ?
+    // Added await to the Asynchronous function using the try-catch method
     document.getElementById("solveRoom3").addEventListener("click", async () => {
     try {
         const res = await fetch('directions.json') 
@@ -35,11 +40,13 @@ document.addEventListener("DOMContentLoaded", () => {
             
 function findMostRecentBook(books) {
     // 🪲 Bug: Logic error
+// Changed code to look for the mostRecent book instead of the oldest book
     return books.reduce((mostRecent, book) => new Date(book.published) > new Date(mostRecent.published) ? book : mostRecent);
 }
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
+    // Added a filter to intersect the sets
     const intersection = new Set([...setA].filter(x => setB.has(x)));
     return intersection;
 }
@@ -47,9 +54,9 @@ function findIntersection(setA, setB) {
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
         // 🪲 Bug: No delay
+        // Added await to the new Promise
         await new Promise(resolve => setTimeout(resolve, 1000));
         console.log(`Navigating: ${direction.step}`);
     }
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
 }
-
