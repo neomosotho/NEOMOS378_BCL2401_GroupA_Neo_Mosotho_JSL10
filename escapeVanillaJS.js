bc442d923bfc55feb4586d33365629544813d392
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-04-18 11:32:57 +0200

    Fixing the intersection

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -30,8 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (err) {
         console.error("Error:",err)
     }
-}
     });
+});
             
 
 function findMostRecentBook(books) {
@@ -41,7 +41,7 @@ function findMostRecentBook(books) {
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
-    const intersection = new Set([...setA]);
+    const intersection = new Set([...setA].filter(x => setB.has(x)));
     return intersection;
 }
 
